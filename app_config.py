cec5228ef2ebe2d9316cbd619f4d753dbc73a994
Joey Coleman <joey@joeycoleman.ca>
2016-07-23 23:45:36 -0400

    app_config for ThePublicRecord.ca

--- app_config.py
+++ app_config.py
@@ -15,29 +15,26 @@ NAMES
 """
 # Project name to be used in urls
 # Use dashes, not underscores!
-PROJECT_SLUG = 'lunchbox-test'
+PROJECT_SLUG = 'ThePublicRecord-ca-lunchbox'
 
 # Project name to be used in file paths
 PROJECT_FILENAME = 'lunchbox'
 
 # The name of the repository containing the source
 REPOSITORY_NAME = 'lunchbox'
-GITHUB_USERNAME = 'nprapps'
+GITHUB_USERNAME = 'joeycoleman'
 REPOSITORY_URL = 'git@github.com:%s/%s.git' % (GITHUB_USERNAME, REPOSITORY_NAME)
 REPOSITORY_ALT_URL = None # 'git@bitbucket.org:nprapps/%s.git' % REPOSITORY_NAME'
 
-DEV_CONTACT = 'EDIT THIS IN APP_CONFIG.PY'
+DEV_CONTACT = '@JoeyColeman'
 
 """
 DEPLOYMENT
 """
-PRODUCTION_S3_BUCKET = 'apps.npr.org'
-STAGING_S3_BUCKET = 'stage-apps.npr.org'
-DEFAULT_MAX_AGE = 20
 
-FILE_SERVER_USER = 'ubuntu'
-FILE_SERVER = 'tools.apps.npr.org'
-FILE_SERVER_PATH = '~/www'
+FILE_SERVER_USER = 'lunchbox'
+FILE_SERVER = 'lunchbox.thepublicrecord.ca'
+FILE_SERVER_PATH = '/home/joeycoleman/webapps/tpr_lunchbox'
 
 # These variables will be set at runtime. See configure_targets() below
 S3_BUCKET = None
